f7e0d21bab0e3475fe2b712a53d680d6783e6c3e
NADA <NADA@C1>
2024-04-04 11:49:42 +0000

    Ajoutez des fichiers projet.

---
+++ pickCards_wpf_version/CardPicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pickCards_wpf_version
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickCards[i] = RandomValue() + "of" + RandomSuit();
+            }
+            return pickCards;
+        }
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
+
+        }
+
+        
+    }
+}
